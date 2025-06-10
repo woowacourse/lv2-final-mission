a4626647015b7ff8741210c0c8f9ebba34146a98
CheChe903 <cheche903@naver.com>
2025-06-10 14:56:15 +0900

    feat(WaitingLine): 대기 순번 구하는 로직 구현 및 단위 테스트 구현

---
+++ src/test/java/finalmission/domain/WaitingLineTest.java
@@ -0,0 +1,76 @@
+package finalmission.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+class WaitingLineTest {
+
+    @Test
+    void 대기에_멤버를_추가한다() {
+
+        // given
+        WaitingLine waitingLine = new WaitingLine(1L, new ArrayList<>());
+
+        // when
+        waitingLine.addMember(new Member(1L, "email", "name", "password"));
+
+        // then
+        assertThat(waitingLine.getWaiting().size()).isEqualTo(1);
+    }
+
+    @Test
+    void 대기에서_특정멤버가_있다면_true를_반환한다() {
+
+        // given
+        WaitingLine waitingLine = new WaitingLine(1L, new ArrayList<>());
+        Member member = new Member(1L, "email", "name", "password");
+
+        // when
+        waitingLine.addMember(member);
+
+        // then
+        assertThat(waitingLine.hasMember(member)).isTrue();
+    }
+
+    @Test
+    void 대기에서_특정멤버가_없다면_false를_반환한다() {
+
+        // given
+        WaitingLine waitingLine = new WaitingLine(1L, new ArrayList<>());
+        Member member = new Member(1L, "email", "name", "password");
+        Member anotherMember = new Member(2L, "email", "name", "password");
+
+        // when
+        waitingLine.addMember(member);
+
+        // then
+        assertThat(waitingLine.hasMember(anotherMember)).isFalse();
+    }
+
+    @Test
+    void 특정멤버의_대기_순서를_찾는다() {
+
+        // given
+        Member member1 = new Member(1L, "email", "name", "password");
+        Member member2 = new Member(2L, "email", "name", "password");
+        Member member3 = new Member(3L, "email", "name", "password");
+        Member member4 = new Member(4L, "email", "name", "password");
+        Member member5 = new Member(5L, "email", "name", "password");
+
+        WaitingLine waitingLine = new WaitingLine(1L, new ArrayList<>());
+
+        waitingLine.addMember(member1);
+        waitingLine.addMember(member2);
+        waitingLine.addMember(member3);
+        waitingLine.addMember(member4);
+        waitingLine.addMember(member5);
+        // when
+        int sequence = waitingLine.getSequenceByMember(member5);
+
+        // then
+        assertThat(sequence).isEqualTo(5);
+    }
+
+}
