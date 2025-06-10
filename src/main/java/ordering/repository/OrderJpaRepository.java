29c90874405ded580fc19eb646025919ab2fa7d2
JO-eusan <goodsan0330@gmail.com>
2025-06-10 16:18:47 +0900

    feat: 사용자 발주 내역 조회

--- src/main/java/ordering/repository/OrderJpaRepository.java
+++ src/main/java/ordering/repository/OrderJpaRepository.java
@@ -1,8 +1,10 @@
 package ordering.repository;
 
+import java.util.List;
 import ordering.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderJpaRepository extends JpaRepository<Order, Long> {
 
+    List<Order> findByUserId(Long userId);
 }
