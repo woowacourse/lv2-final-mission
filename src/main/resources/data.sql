9d008f13b4d1a6d337923744480ae2cc361aa318
jhpark1227 <junhyeok0246@naver.com>
2025-06-10 17:23:44 +0900

    chore: 더미데이터 수정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO MEMBER (email, name, password)
+VALUES ('email1@domain.com', 'name1', '1234');
+
+INSERT INTO TOILET (position)
+VALUES ('루터회관 14층 1번칸'),
+       ('루터회관 14층 2번칸'),
+       ('루터회관 14층 3번칸');
