2fcccdc3a05fe06c53029948891cb9d22a7c31b0
minSsan <msp1125@hanyang.ac.kr>
2025-06-10 13:57:15 +0900

    feat: 고객 엔티티 추가

---
+++ src/main/java/finalmission/user/domain/Customer.java
@@ -0,0 +1,48 @@
+package finalmission.user.domain;
+
+import finalmission.user.domain.vo.Email;
+import finalmission.user.domain.vo.Name;
+import finalmission.user.domain.vo.Password;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(uniqueConstraints =
+    @UniqueConstraint(columnNames = {"email"})
+)
+@Getter
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    @AttributeOverrides(
+            @AttributeOverride(name = "value", column = @Column(name = "name"))
+    )
+    private Name name;
+
+    @Embedded
+    @AttributeOverrides(
+            @AttributeOverride(name = "value", column = @Column(name = "email"))
+    )
+    private Email email;
+
+    @Embedded
+    @AttributeOverrides(
+            @AttributeOverride(name = "value", column = @Column(name = "password"))
+    )
+    private Password password;
+}
---
+++ src/main/java/finalmission/user/domain/vo/Name.java
@@ -0,0 +1,16 @@
+package finalmission.user.domain.vo;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record Name(String value) {
+    public Name {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("이름이 설정되지 않았습니다.");
+        }
+
+        if (value.length() > 10) {
+            throw new IllegalArgumentException("이름은 최대 10글자까지 설정 가능합니다.");
+        }
+    }
+}
