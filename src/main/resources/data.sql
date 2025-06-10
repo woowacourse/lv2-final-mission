5ed3c3fee56d8592b976ba5ff777eabf091025d2
Hacanna42 <dtmdgus901@gmail.com>
2025-06-10 17:11:51 +0900

    chore: data.sql 더미 데이터 초기화

---
+++ src/main/resources/data.sql
@@ -0,0 +1,58 @@
+-- LINE
+INSERT INTO Line (number) VALUES (2);
+
+-- STATION
+INSERT INTO Station (name, line_number) VALUES ('시청', 2);
+INSERT INTO Station (name, line_number) VALUES ('을지로입구', 2);
+INSERT INTO Station (name, line_number) VALUES ('을지로3가', 2);
+INSERT INTO Station (name, line_number) VALUES ('을지로4가', 2);
+INSERT INTO Station (name, line_number) VALUES ('동대문역사문화공원', 2);
+INSERT INTO Station (name, line_number) VALUES ('신당', 2);
+INSERT INTO Station (name, line_number) VALUES ('상왕십리', 2);
+INSERT INTO Station (name, line_number) VALUES ('왕십리', 2);
+INSERT INTO Station (name, line_number) VALUES ('한양대', 2);
+INSERT INTO Station (name, line_number) VALUES ('뚝섬', 2);
+INSERT INTO Station (name, line_number) VALUES ('성수', 2);
+INSERT INTO Station (name, line_number) VALUES ('건대입구', 2);
+INSERT INTO Station (name, line_number) VALUES ('구의', 2);
+INSERT INTO Station (name, line_number) VALUES ('강변', 2);
+INSERT INTO Station (name, line_number) VALUES ('잠실나루', 2);
+INSERT INTO Station (name, line_number) VALUES ('잠실', 2);
+INSERT INTO Station (name, line_number) VALUES ('잠실새내', 2);
+INSERT INTO Station (name, line_number) VALUES ('종합운동장', 2);
+INSERT INTO Station (name, line_number) VALUES ('삼성', 2);
+INSERT INTO Station (name, line_number) VALUES ('선릉', 2);
+INSERT INTO Station (name, line_number) VALUES ('역삼', 2);
+INSERT INTO Station (name, line_number) VALUES ('강남', 2);
+INSERT INTO Station (name, line_number) VALUES ('교대', 2);
+INSERT INTO Station (name, line_number) VALUES ('서초', 2);
+INSERT INTO Station (name, line_number) VALUES ('방배', 2);
+INSERT INTO Station (name, line_number) VALUES ('사당', 2);
+INSERT INTO Station (name, line_number) VALUES ('낙성대', 2);
+INSERT INTO Station (name, line_number) VALUES ('서울대입구', 2);
+INSERT INTO Station (name, line_number) VALUES ('봉천', 2);
+INSERT INTO Station (name, line_number) VALUES ('신림', 2);
+INSERT INTO Station (name, line_number) VALUES ('신대방', 2);
+INSERT INTO Station (name, line_number) VALUES ('구로디지털단지', 2);
+INSERT INTO Station (name, line_number) VALUES ('대림', 2);
+INSERT INTO Station (name, line_number) VALUES ('신도림', 2);
+INSERT INTO Station (name, line_number) VALUES ('문래', 2);
+INSERT INTO Station (name, line_number) VALUES ('영등포구청', 2);
+INSERT INTO Station (name, line_number) VALUES ('당산', 2);
+INSERT INTO Station (name, line_number) VALUES ('합정', 2);
+INSERT INTO Station (name, line_number) VALUES ('홍대입구', 2);
+INSERT INTO Station (name, line_number) VALUES ('신촌', 2);
+INSERT INTO Station (name, line_number) VALUES ('이대', 2);
+INSERT INTO Station (name, line_number) VALUES ('아현', 2);
+INSERT INTO Station (name, line_number) VALUES ('충정로', 2);
+
+-- SUBWAY
+INSERT INTO Subway (id, line_number) VALUES (1, 2);
+INSERT INTO Subway (id, line_number) VALUES (2, 2);
+INSERT INTO Subway (id, line_number) VALUES (3, 2);
+INSERT INTO Subway (id, line_number) VALUES (4, 2);
+INSERT INTO Subway (id, line_number) VALUES (5, 2);
+INSERT INTO Subway (id, line_number) VALUES (6, 2);
+INSERT INTO Subway (id, line_number) VALUES (7, 2);
+INSERT INTO Subway (id, line_number) VALUES (8, 2);
+INSERT INTO Subway (id, line_number) VALUES (9, 2);
