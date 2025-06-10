21fc6fb127ac02f195462c2b103abbf97dcea237
BWY <quantumjump10@gmail.com>
2025-06-10 17:24:06 +0900

    feat: jwt 토큰 발급 및 추출 기능 추가

---
+++ src/main/java/finalmission/external/jwt/JwtTokenExtractor.java
@@ -0,0 +1,26 @@
+package finalmission.external.jwt;
+
+import finalmission.auth.exception.AuthException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+
+public class JwtTokenExtractor {
+
+    private JwtTokenExtractor() {}
+
+    public static String extract(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            throw new AuthException("[ERROR] 쿠키가 존재하지 않습니다.", HttpStatus.UNAUTHORIZED);
+        }
+
+        return Arrays.stream(cookies)
+                .filter(cookie -> "token".equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new AuthException("[ERROR] 토큰이 존재하지 않습니다.", HttpStatus.UNAUTHORIZED));
+    }
+}
