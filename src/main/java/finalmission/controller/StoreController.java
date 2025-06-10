3ee2dd98199305aacfd159d34e147e85ea7a6c91
CheChe903 <cheche903@naver.com>
2025-06-10 15:51:41 +0900

    feat(StoreController): 엔드 포인트 복수로 변경

--- src/main/java/finalmission/controller/StoreController.java
+++ src/main/java/finalmission/controller/StoreController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/store")
+@RequestMapping("/stores")
 public class StoreController {
 
     private final StoreService storeService;
