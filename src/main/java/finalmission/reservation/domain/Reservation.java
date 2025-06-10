59d371537244f593d5259102d863a40affd59d9d
minSsan <msp1125@hanyang.ac.kr>
2025-06-10 14:20:46 +0900

    feat: 예약 (Reservation) 엔티티 추가

---
+++ src/main/java/finalmission/reservation/domain/Reservation.java
@@ -0,0 +1,29 @@
+package finalmission.reservation.domain;
+
+import finalmission.user.domain.Customer;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "information_id", nullable = false)
+    private ReservationInformation reservationInformation;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+}
