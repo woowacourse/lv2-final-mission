6dd1d4ced036630f6c0b91dacf6aa5d546ab10e2
Jin409 <sualng123@gmail.com>
2025-06-10 17:19:04 +0900

    chore: 스웨거 추가

--- src/main/java/finalmission/presentation/controller/MemberController.java
+++ src/main/java/finalmission/presentation/controller/MemberController.java
@@ -2,6 +2,8 @@ package finalmission.presentation.controller;
 
 import finalmission.dto.MemberRegisterDto;
 import finalmission.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "회원 관련 API")
 @RestController
 @RequestMapping("/member")
 @RequiredArgsConstructor
@@ -18,11 +21,13 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입")
     @PostMapping
     public void signUp(@RequestBody MemberRegisterDto memberRegisterDto) {
         memberService.signUp(memberRegisterDto);
     }
 
+    @Operation(summary = "회원가입 시 제공하는 랜덤 이름 (기본 제공값 10개)")
     @GetMapping("/random-names")
     public List<String> getRandomNames(@RequestParam(name = "quantity") int quantity) {
         return memberService.getRandomNames(quantity);
