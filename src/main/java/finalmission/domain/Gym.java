fab749eca647269b6d4e5cb2e49528d1a2bc63d6
Minuring <mw9505@gmail.com>
2025-06-10 13:34:12 +0900

    feat: Gym 이름 검증

--- src/main/java/finalmission/domain/Gym.java
+++ src/main/java/finalmission/domain/Gym.java
@@ -5,4 +5,13 @@ public record Gym(
     Address address
 ) {
 
+    public static final int MIN_NAME_LENGTH = 3;
+    public static final int MAX_NAME_LENGTH = 10;
+
+    public Gym {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+            var message = String.format("헬스장 이름은 %d자 이상 %d자 이하여야 합니다.", MIN_NAME_LENGTH, MAX_NAME_LENGTH);
+            throw new IllegalArgumentException(message);
+        }
+    }
 }
--- src/test/java/finalmission/domain/GymTest.java
+++ src/test/java/finalmission/domain/GymTest.java
@@ -1,10 +1,13 @@
 package finalmission.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class GymTest {
 
@@ -13,5 +16,13 @@ class GymTest {
     void newGym() {
         var gym = new Gym("짐박스", new Address("군자로 123-1", "지하 1층"));
         assertThat(gym).isNotNull();
+    }
+
+    @ParameterizedTest
+    @DisplayName("헬스장 이름은 3자 이상 10자 이하여야 한다.")
+    @ValueSource(strings = {"", "헬", "헬스", "헬스헬스헬스헬스헬스헬"})
+    void validateName(String name) {
+        assertThatThrownBy(() -> new Gym(name, new Address("군자로 123-1", "지하 1층")))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
