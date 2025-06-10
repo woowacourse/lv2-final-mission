890f85717d1ec90cf5cfe737743bc40573a2c699
minSsan <msp1125@hanyang.ac.kr>
2025-06-10 14:20:34 +0900

    feat: 예약 가능 날짜/시간/최대 건수 정보를 저장하는 ReservationInformation 엔티티 추가

---
+++ src/main/java/finalmission/reservation/domain/ReservationInformation.java
@@ -0,0 +1,39 @@
+package finalmission.reservation.domain;
+
+import finalmission.restaurant.domain.Restaurant;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(uniqueConstraints =
+    @UniqueConstraint(columnNames = {"restaurant_id", "date", "start_at"})
+)
+@Getter
+@NoArgsConstructor
+public class ReservationInformation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    private LocalDate date;
+
+    private LocalTime startAt;
+
+    private Integer maxCount; // 예약 가능한 최대 건수
+}
