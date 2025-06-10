e9f2c7d629a94df3955475f5bba4bc4355bb0caa
Dompoo <dlckdrms0517@gmail.com>
2025-06-10 15:50:49 +0900

    chore: TODO 작성

--- src/main/java/finalmission/service/RandommerNameGenerator.java
+++ src/main/java/finalmission/service/RandommerNameGenerator.java
@@ -21,6 +21,11 @@ public class RandommerNameGenerator implements NameGenerator {
     }
 
     @Override
+    /*
+    TODO : 외부 API 예외 상황 처리 추가 필요
+    TODO : 한국어로된 이름을 생성할 필요가 있음. -> 외부 API 찾아보고 없으면 구현해야 할듯
+    ex) 달콤한 햄스터, 꿋꿋한 춘식이, 놀라운 기린
+     */
     public String generate() {
         String result = restClient.get()
                 .header("X-Api-Key", apiKey)
