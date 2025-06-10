48111e7c8b9e309506445b7af1e4d48a0827476d
LeeJaeHoon <dlwogns3413@naver.com>
2025-06-10 14:17:05 +0900

    chore: 초기 데이터 세팅

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO member(name, email, password, role)
+VALUES ('서프', 'user@gmail.com', 'password', 'USER');
+INSERT INTO refrigerator(name)
+VALUES ('우테코');
+INSERT INTO monster_energy_stock(monster_energy, stock, refrigerator_id)
+VALUES ('ULTRA', 100, 1);
+INSERT INTO monster_energy_stock(monster_energy, stock, refrigerator_id)
+VALUES ('ULTRA_CITRA', 100, 1);
+INSERT INTO monster_energy_stock(monster_energy, stock, refrigerator_id)
+VALUES ('MANGO_LOCO', 100, 1);
+INSERT INTO monster_energy_stock(monster_energy, stock, refrigerator_id)
+VALUES ('ZERO_SUGAR', 100, 1);
