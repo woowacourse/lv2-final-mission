fdadcaa42d5dedc159f2e234a6e7736eb0b91fc9
ChoiDongjun <adong0808@naver.com>
2025-06-14 20:46:56 +0900

    chore: 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
-INSERT INTO RESTAURANT(address, name, menus) VALUES ('address', 'matzip', 'menu')
+INSERT INTO RESTAURANT(address, name, menus) VALUES ('address', 'matzip', 'menu');
 
-INSERT INTO MEMBER(name, email, password) VALUES ('eve', 'eve@example.com', 'password')
+INSERT INTO MEMBER(name, email, password) VALUES ('eve', 'eve@example.com', 'password');
+INSERT INTO MEMBER(name, email, password) VALUES ('pop', 'pop@example.com', 'password');
+
+INSERT INTO RESERVATION(reservation_date_time, member_id, restaurant_id, personnel)
+VALUES ('2025-06-11T10:00:00', 1L, 1L, 2);
