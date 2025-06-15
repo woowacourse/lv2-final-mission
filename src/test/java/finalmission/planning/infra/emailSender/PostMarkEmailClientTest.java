6a2121910d14045a9091facf097e90d902f706ee
YuyoungRhee <bbclara@naver.com>
2025-06-16 00:22:52 +0900

    feat: 이메일 발송 외부 클라이언트 구현

---
+++ src/test/java/finalmission/planning/infra/emailSender/PostMarkEmailClientTest.java
@@ -0,0 +1,35 @@
+package finalmission.planning.infra.emailSender;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import finalmission.planning.infra.EmailRequest;
+import finalmission.planning.ui.IntegrationTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class PostMarkEmailClientTest extends IntegrationTest {
+
+    @Autowired
+    PostMarkEmailClient postMarkEmailClient;
+
+    @Disabled // 실제 API를 호출하는 테스트 - 확인 필요 시 활성화
+    @DisplayName("이메일을 전송한다.")
+    @Test
+    void sendEmail_success() {
+        // given
+        EmailRequest emailRequest = new EmailRequest("wooung@duksung.ac.kr",
+                "wooung@duksung.ac.kr",
+                "Hello from Postmark",
+                "<strong>Hello</strong> dear Postmark user.",
+                "outbound");
+
+        // when
+        postMarkEmailClient.sendEmail(emailRequest);
+
+        // then
+        // 실제 이메일함 확인
+    }
+
+}
