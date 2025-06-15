101db66b4159692cb416dd76680d1d259922b674
jhpark1227 <junhyeok0246@naver.com>
2025-06-16 03:06:50 +0900

    chore: property 및 더미데이터 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO member (email, nickname, password)
+VALUES ('email1@domain.com', 'nickname1', '1234'),
+       ('email2@domain.com', 'nickname2', '1234');
+
+INSERT INTO toilet (position)
+VALUES ('루터회관 14층 1번'),
+       ('루터회관 14층 2번'),
+       ('루터회관 14층 3번');
