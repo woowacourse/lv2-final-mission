9882c841ea22c7cd3efe3bad28de47e40bf04a3a
jhpark1227 <junhyeok0246@naver.com>
2025-06-16 02:21:16 +0900

    test: ToiletService 단위테스트 작성

--- src/main/java/finalmission/toilet/infrastructure/ToiletRepository.java
+++ src/main/java/finalmission/toilet/infrastructure/ToiletRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface ToiletRepository extends Repository<Toilet, Long> {
+    Toilet save(Toilet toilet);
+
     List<Toilet> findAll();
 
     Optional<Toilet> findById(Long id);
