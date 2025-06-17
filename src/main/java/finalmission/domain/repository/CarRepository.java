3f1a3682f749dc51ebe54743a5c108b3e0b97306
Yehyeok Bang <qkddpgur318@gmail.com>
2025-06-17 14:42:52 +0900

    fix: 공휴일 API 사용 중지

--- src/main/java/finalmission/domain/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package finalmission.domain.repository;
-
-import finalmission.domain.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
