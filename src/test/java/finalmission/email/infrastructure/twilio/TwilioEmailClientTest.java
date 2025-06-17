62d2146c0548a2062740d1821095bd9f122b6564
threepebbles <threepebbles@naver.com>
2025-06-17 15:08:44 +0900

    test: 주석 추가

--- src/test/java/finalmission/email/infrastructure/twilio/TwilioEmailClientTest.java
+++ src/test/java/finalmission/email/infrastructure/twilio/TwilioEmailClientTest.java
@@ -2,10 +2,12 @@ package finalmission.email.infrastructure.twilio;
 
 import finalmission.email.domain.EmailClient;
 import finalmission.email.infrastructure.twilio.dto.SendEmailRequest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled // Sendgrid 이메일 전송 기능은 실제로 이메일을 전송하므로 테스트를 비활성화합니다.
 @SpringBootTest
 class TwilioEmailClientTest {
 
