462bc21ca29324031d676278e776100179a0230c
threepebbles <threepebbles@naver.com>
2025-06-17 22:27:02 +0900

    test: 예약 대기 알림 이메일 전송 테스트 추가

--- src/test/java/finalmission/email/infrastructure/twilio/TwilioEmailClientTest.java
+++ src/test/java/finalmission/email/infrastructure/twilio/TwilioEmailClientTest.java
@@ -2,6 +2,11 @@ package finalmission.email.infrastructure.twilio;
 
 import finalmission.email.domain.EmailClient;
 import finalmission.email.infrastructure.twilio.dto.SendEmailRequest;
+import finalmission.reservation.domain.ReservationSlot;
+import finalmission.reservation.domain.ReservationTime;
+import finalmission.restaurant.domain.Restaurant;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +20,32 @@ class TwilioEmailClientTest {
     private EmailClient emailClient;
 
     @Test
-    void 이메일_전송_테스트() {
+    void 음식점_예약_성공_이메일_전송_테스트() {
         final SendEmailRequest request = SendEmailRequest.confirmReservation("threepebbles@naver.com");
+        emailClient.sendEmail(request);
+    }
+
+    @Test
+    void 음식점_예약_대기_알림_이메일_전송_테스트() {
+        // given
+        final String emailTo = "threepebbles@naver.com";
+        final Restaurant restaurant = new Restaurant(
+                "차이나 스토리",
+                "역시 차스입니다.",
+                "서울시 어딘가1",
+                "02-1234-5678",
+                20
+        );
+        final ReservationTime reservationTime = new ReservationTime(LocalTime.of(12, 0));
+        final ReservationSlot reservationSlot = new ReservationSlot(
+                LocalDate.now().plusDays(1),
+                reservationTime,
+                restaurant,
+                20
+        );
+
+        // when
+        final SendEmailRequest request = SendEmailRequest.alarmForWaiting(emailTo, reservationSlot);
         emailClient.sendEmail(request);
     }
 }
