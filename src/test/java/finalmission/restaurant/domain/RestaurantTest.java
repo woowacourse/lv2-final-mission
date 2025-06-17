d40da613f1bd7e0e264f4368f473dfb55410568a
threepebbles <threepebbles@naver.com>
2025-06-17 15:07:41 +0900

    test: 잘못된 given 수정

--- src/test/java/finalmission/restaurant/domain/RestaurantTest.java
+++ src/test/java/finalmission/restaurant/domain/RestaurantTest.java
@@ -10,7 +10,7 @@ class RestaurantTest {
     @Test
     void 이름이_null이면_예외가_발생한다() {
         // given
-        final String name = "차이나 스토리";
+        final String name = null;
         final String description = "12년 전통의 잠실 대표 중식당입니다";
         final String place = "서울 송파구 신천동 7-22";
         final String phoneNumber = "0507-1397-4624";
