1a3732be5caebec9b4438dd89550c6cec1594d3c
LeeJaeHoon <dlwogns3413@naver.com>
2025-06-18 18:45:55 +0900

    test: reservation 서비스 delete 메서드 테스트

--- src/test/java/finalmission/application/ReservationServiceTest.java
+++ src/test/java/finalmission/application/ReservationServiceTest.java
@@ -1,11 +1,13 @@
 package finalmission.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.tuple;
 
 import finalmission.application.dto.request.CreateReservationRequest;
 import finalmission.application.dto.request.UpdateReservationRequest;
 import finalmission.application.dto.response.CreateReservationResponse;
+import finalmission.application.support.exception.BusinessRuleViolationException;
 import finalmission.domain.Email;
 import finalmission.domain.Member;
 import finalmission.domain.MemberRepository;
@@ -98,5 +100,53 @@ class ReservationServiceTest extends AbstractServiceIntegrationTest {
                 .containsExactly(MonsterEnergy.ULTRA, 9);
         assertThat(monsterEnergyStock.getStock())
                 .isEqualTo(1);
+    }
+
+    @Test
+    void 예약의_재고를_변경할때_예약을_생성한_사용자가_아니라면_예외가_발생한다() {
+        //given
+        Member member = memberRepository.save(Member.create(new Email("user@gmail.com"), "서프", "password", Role.USER));
+        Refrigerator refrigerator = new Refrigerator("우테코");
+        new MonsterEnergyStock(MonsterEnergy.ULTRA, 10, refrigerator);
+        refrigeratorRepository.save(refrigerator);
+        Reservation reservation = reservationRepository.save(
+                Reservation.create(
+                        MonsterEnergy.ULTRA,
+                        8,
+                        LocalDateTime.now().plusDays(3),
+                        refrigerator,
+                        member,
+                        LocalDateTime.now()
+                )
+        );
+
+        //when & then
+        assertThatThrownBy(() -> reservationService.update(999L, reservation.getId(), new UpdateReservationRequest(9)))
+                .isInstanceOf(BusinessRuleViolationException.class)
+                .hasMessage("예약을 생성한 사용자만 수정이 가능합니다.");
+    }
+
+    @Test
+    void 예약을_생성하지_않은_사용자가_예약을_삭제할때_예외가_발생한다() {
+        //given
+        Member member = memberRepository.save(Member.create(new Email("user@gmail.com"), "서프", "password", Role.USER));
+        Refrigerator refrigerator = new Refrigerator("우테코");
+        new MonsterEnergyStock(MonsterEnergy.ULTRA, 10, refrigerator);
+        refrigeratorRepository.save(refrigerator);
+        Reservation reservation = reservationRepository.save(
+                Reservation.create(
+                        MonsterEnergy.ULTRA,
+                        8,
+                        LocalDateTime.now().plusDays(3),
+                        refrigerator,
+                        member,
+                        LocalDateTime.now()
+                )
+        );
+
+        //when & then
+        assertThatThrownBy(() -> reservationService.delete(999L, reservation.getId()))
+                .isInstanceOf(BusinessRuleViolationException.class)
+                .hasMessage("예약을 생성한 사용자만 삭제가 가능합니다.");
     }
 }
