38c3a488d8afcccc6e128f4a960b6d5104760060
LeeJaeHoon <dlwogns3413@naver.com>
2025-06-18 17:24:38 +0900

    test: 사용자 이름없을때 이름 랜덤 생성 로직 테스트

--- src/test/java/finalmission/application/AbstractServiceIntegrationTest.java
+++ src/test/java/finalmission/application/AbstractServiceIntegrationTest.java
@@ -1,11 +1,16 @@
 package finalmission.application;
 
+import finalmission.infrastructure.RandomNameClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 public abstract class AbstractServiceIntegrationTest {
+
+    @MockitoBean
+    protected RandomNameClient randomNameClient;
 }
