22d5e61708a40a2b8d86a208393f49294aa8766f
CheChe903 <cheche903@naver.com>
2025-06-18 15:26:13 +0900

    feat: 사용하지 않는 인터셉터 삭제

--- src/main/java/finalmission/config/WebMvcConfiguration.java
+++ src/main/java/finalmission/config/WebMvcConfiguration.java
@@ -1,12 +1,10 @@
 package finalmission.config;
 
-import finalmission.infra.auth.AuthenticationInterceptor;
 import finalmission.infra.auth.AuthenticationPrincipalResolver;
 import finalmission.service.AuthService;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -21,11 +19,5 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthenticationPrincipalResolver(authService));
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthenticationInterceptor(authService))
-                .addPathPatterns("/admin/**", "/reservation-mine");
     }
 }
