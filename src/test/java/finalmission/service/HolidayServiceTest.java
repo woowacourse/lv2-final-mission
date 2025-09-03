d50bde354fa77ed54134f01f8dff313ba913afa0
supernovaMK <minkikim2002@gmail.com>
2025-09-03 15:23:20 +0900

    test: H2 데이터베이스 설정 및 테스트 프로파일 추가, 테스트 클래스 이름 수정

--- src/test/java/finalmission/service/HolidayServiceTest.java
+++ src/test/java/finalmission/service/HolidayServiceTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-public class HolydayServiceTest {
+@ActiveProfiles("test")
+public class HolidayServiceTest {
 
     @Autowired
     HolidayService holidayService;
