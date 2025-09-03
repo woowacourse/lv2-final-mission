f903f7415e8db3ac7a1b4d99fe371cf02246366c
YuyoungRhee <bbclara@naver.com>
2025-09-03 15:03:47 +0900

    chore: 헬스체크 의존성 추가

--- src/main/java/finalmission/planning/config/WebConfig.java
+++ src/main/java/finalmission/planning/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(basicAuthorizationInterceptor)
-                .excludePathPatterns("/login");
+                .excludePathPatterns("/login", "/actuator/**", "/favicon.ico");
         registry.addInterceptor(adminAuthorizationInterceptor)
                 .addPathPatterns("/admin/**");
     }
